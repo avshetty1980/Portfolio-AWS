bfbaf9c152aed43f1a71e73b368990d540f7fe38
Ash Shetty <avshetty1980@gmail.com>
2021-11-10 16:10:14 +0000

    favicon and name on tab

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,15 @@
 import Theme from '../styles/theme';
+import Head from "next/head"
+
+const prefix = '/Portfolio-AWS';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href={prefix+"/favicon.ico"} />
+        <title>Ash Shetty</title>
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
